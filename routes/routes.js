550965568d9bc7f8c70b64dd5883dac54cde75ed
edadvisory <edadvisorybysakina@gmail.com>
2026-03-06 06:41:23 +0500

    Updated  routes.js with blogs routes

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const testimonialsController = require('../controllers/testimonialsController');
 const bookingsController = require('../controllers/bookingsController');
 const contactController = require('../controllers/contactController');
+const blogsController = require('../controllers/blogsController');
 
 // Testimonials routes
 router.post('/testimonials', testimonialsController.upload.single('profilePicture'), testimonialsController.createTestimonial);
@@ -12,6 +13,10 @@ router.get('/testimonials', testimonialsController.getAllTestimonials);
 // Bookings routes
 router.post('/bookings', bookingsController.createBooking);
 router.get('/bookings', bookingsController.getAllBookings);
+
+// Blogs routes
+router.get('/blogs', blogsController.getBlogs);
+router.get('/blogs/:id', blogsController.getBlog);
 
 // Handle contact form submission
 router.post('/contact', contactController.handleContactForm);
